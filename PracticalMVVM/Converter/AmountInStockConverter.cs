a5f40fc521fab1b2112d29b4b4ef8beb56279125
Bartek Kawik <bartek.kawik@gmail.com>
2019-09-16 23:21:12 -0400

    Converters

---
+++ PracticalMVVM/Converter/AmountInStockConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PracticalMVVM.Converter
+{
+    public class AmountInStockConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if ((int)value >= 1000)
+                return ((int)value / 1000.0).ToString() + " k";
+            else
+                return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PracticalMVVM/Converter/ImageConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace PracticalMVVM.Converter
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri("/PracticalMVVM;component/Images/coffee" + value + ".jpg", UriKind.Relative);
+            img.EndInit();
+            return img;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
