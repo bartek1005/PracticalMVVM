acbc262cd8bd79b252d5a03067e48d17b0bb0c37
Bartek Kawik <bartek.kawik@gmail.com>
2019-09-17 21:54:52 -0400

    MVVM Refactoring

---
+++ PracticalMVVM/ViewModelLocator.cs
@@ -0,0 +1,31 @@
+﻿using PracticalMVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalMVVM
+{
+    public class ViewModelLocator
+    {
+        private static CoffeeOverviewViewModel coffeeOverviewViewModel = new CoffeeOverviewViewModel();
+        private static CoffeeDetailViewModel coffeeDetailViewModel = new CoffeeDetailViewModel();
+
+        public CoffeeOverviewViewModel CoffeeOverviewViewModel
+        {
+            get
+            {
+                return coffeeOverviewViewModel;
+            }
+        }
+
+        public CoffeeDetailViewModel CoffeeDetailViewModel
+        {
+            get
+            {
+                return coffeeDetailViewModel;
+            }
+        }
+    }
+}
